8f2da25aaef5cedb3220a55dc637438396699398
Srigi <295197+srigi@users.noreply.github.com>
2025-08-03 17:41:47 +0200

    Remove unneeded ENVs

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,8 +1,5 @@
 import { z } from 'zod';
 
-const envSchema = z.object({
-  API_KEY: z.string().min(1, 'API_KEY environment variable is required'),
-  SEARCH_ENGINE_ID: z.string().min(1, 'SEARCH_ENGINE_ID environment variable is required'),
-});
+const envSchema = z.object({});
 
 export const env = envSchema.parse(process.env);
