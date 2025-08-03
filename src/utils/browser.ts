dca27080eac6fe36a10ce5ba6f1a2903e7ad61cc
Srigi <295197+srigi@users.noreply.github.com>
2025-08-03 11:32:46 +0200

    Extract getBrowser() into shared util

---
+++ src/utils/browser.ts
@@ -0,0 +1,22 @@
+import { Browser, chromium } from 'playwright';
+
+import { tryCatch } from './tryCatch';
+
+export class BrowserLaunchError extends Error {
+  name = 'BrowserLaunchError';
+}
+
+let browserInstance: Browser | undefined;
+
+export async function getBrowser() {
+  if (browserInstance == null) {
+    const [launchErr, browser] = await tryCatch(chromium.launch({ headless: true }));
+    if (launchErr) {
+      throw new BrowserLaunchError(`Failed to launch browser: ${launchErr.message}`, { cause: launchErr });
+    }
+
+    browserInstance = browser;
+  }
+
+  return browserInstance;
+}
